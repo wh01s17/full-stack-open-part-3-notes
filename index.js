c75c62ad74769c682e754d801d69acf0c140aca2
wh01s17 <jor.perezo@hotmail.com>
2025-01-13 11:30:34 -0300

    add frontend production

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
